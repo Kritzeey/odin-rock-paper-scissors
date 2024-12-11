6e117c626dbd06e92678fbd1f584be05c5c1a1e2
Valerian Hizkia <valhize@Valerians-MacBook-Pro.local>
2024-12-11 21:00:14 +0700

    Added game logic function

--- script.js
+++ script.js
@@ -14,3 +14,56 @@ function getComputerChoice() {
 function getHumanChoice() {
     return prompt("Enter your choice!")
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase()
+    computerChoice = computerChoice.toLowerCase()
+
+    if (humanChoice == "rock") {
+        switch (computerChoice) {
+            case "rock":
+                break;
+            case "paper":
+                computerScore++
+                break;
+            case "scissors":
+                humanScore++
+                break;
+        }
+    } else if (humanChoice == "paper") {
+        switch (computerChoice) {
+            case "rock":
+                humanScore++
+                break;
+            case "paper":
+                break;
+            case "scissors":
+                computerScore++
+                break;
+        }
+    } else if (humanChoice == "scissors") {
+        switch (computerChoice) {
+            case "rock":
+                computerScore++
+                break;
+            case "paper":
+                humanScore++
+                break;
+            case "scissors":
+                break;
+        }
+    }
+
+    console.log(`You chose ${humanChoice}!\n
+Computer chose ${computerChoice}!`)
+    console.log(`You got ${humanScore} points!\n
+Computer got ${computerScore} points!`)
+}
+
+
+const humanSelection = getHumanChoice()
+const computerSelection = getComputerChoice()
+playRound(humanSelection, computerSelection)
